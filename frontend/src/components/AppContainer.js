3e9c6a229764b68d9e020f4844c633589da047b9
Harshitha Reddy.k <harshithareddy.k@Harshithas-MacBook-Air.local>
2024-04-01 23:56:29 -0400

    myprofile changes

--- frontend/src/components/AppContainer.js
+++ frontend/src/components/AppContainer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {HashRouter, Routes, Route} from "react-router-dom";
-
+import FeedPage from "./feed";
 import SignUp from "./signup";
 
 function AppContainer() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<SignUp />} />       
+        <Route path="/" element={<SignUp />} />    
+        <Route path="/newsfeed" element={<FeedPage/>} />   
       </Routes>
     </HashRouter>
   );
