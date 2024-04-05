16f06ff3d9cd241494a69fce4028d57e876038f6
kalyanibiradar <ksbirada@syr.edu>
2024-04-05 18:18:44 -0400

    login updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './components/signup'
 import NewsFeed from './components/NewsFeed';
+import Login from './components/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/newsfeed" element={<NewsFeed />} />
       </Routes>
